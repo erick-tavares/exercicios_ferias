613e822cbc235bf397992e49b4e87f36180fa927
erick-tavares <erick-tavares@hotmail.com>
2019-07-23 11:50:31 -0300

    exercicios 50 e 51

--- ExerciciosFerias/src/Static/TesteConta.java
+++ ExerciciosFerias/src/Static/TesteConta.java
@@ -5,10 +5,10 @@ public class TesteConta {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Conta c1 = new Conta();
-		System.out.println("Número de contas: " + Conta.contador);
+		System.out.println("Número de contas: " + c1.numeroDeConta);
 		
 		Conta c2 = new Conta();
-		System.out.println("Número de contas: " + Conta.contador);
+		System.out.println("Número de contas: " + c2.numeroDeConta);
 		
 	}
 
