3ced38e89825440a2792af85c1ac0323a2ddcce8
erick-tavares <erick-tavares@hotmail.com>
2019-07-23 09:48:05 -0300

    Exercicios 42 e 43

---
+++ ExerciciosFerias/src/arrays/TesteArray.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class TesteArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] numero = new int []{4,6,3,8,9};
+		
+		for (int i = 0; i < numero.length; i++) {
+			numero [i] = i;
+			System.out.println(numero[i]);
+		}
+	}
+
+}
